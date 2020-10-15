0d385affb2228825d6f49171113e7794db4d993e
Guillaume MAIANO <guillaume@maiano.fr>
2020-10-15 12:56:51 +0200

    Basic UI code with no useful anything.

--- SimpleDemoMVVM/Login+SigninVC.swift
+++ SimpleDemoMVVM/Login+SigninVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginSigninVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SimpleDemoMVVM/SigninWebviewVC.swift
@@ -0,0 +1,22 @@
+//
+//  SigninWebview.swift
+//  SimpleDemoMVVM
+//
+//  Created by guillaume MAIANO on 15/10/2020.
+//
+
+import UIKit
+import WebKit
+
+class SigninWebviewVC: UIViewController {
+
+    @IBOutlet weak var signinWebView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
